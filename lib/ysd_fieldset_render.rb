cd97751b259d2ad47e93453c9fa5bc050f3edd83
yurak <yurak.sisa.dream@gmail.com>
2012-06-07 10:00:52 +0200

    first release

---
+++ lib/ysd_fieldset_render.rb
@@ -0,0 +1,63 @@
+module UIFieldSetRender
+  class FieldSetRender
+     
+     #
+     # Initialize
+     #
+     def initialize(name, app)
+
+        @name = name
+        @app = app
+        
+     end
+     
+     #
+     # Renders a resource
+     # @param [String] resource_name
+     #   To get the template
+     # @param [String] subsystem
+     #   To get the template
+     # @param [Hash] options
+     #   To extract information
+     #
+     def render(resource_type, subsystem='', options={})
+
+      template_name = [@name] 
+      template_name << "-#{subsystem}" if subsystem and subsystem.to_s.strip.length > 0       
+      template_name << "-#{resource_type}"
+      template_name = template_name.join
+      
+      #puts "template_name : #{template_name}"
+      
+      template_path = find_template(template_name)      
+      template = Tilt.new(template_path)
+      the_render = template.render(@app, options)                                  
+      
+      #puts "render (#{template_name}) :  #{the_render}"
+      
+      the_render
+     
+     end
+  
+     # Finds the template to render the content
+     #
+     #
+     def find_template(name)
+      
+        # Search in theme path
+        template_path = Themes::ThemeManager.instance.selected_theme.resource_path("#{name}-fieldset-render.erb",'template','ui') 
+         
+        # Search in the project
+        if not template_path
+           path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'views', "#{name}-fieldset-render.erb"))                                 
+           template_path = path if File.exist?(path)
+        end
+         
+        puts "ui template path (#name) : #{template_path}"
+        
+        template_path
+       
+    end #find_template
+     
+  end #FieldSetRender
+end #UIFieldSetRender
---
+++ ysd_ui_fieldset_renders.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name    = "ysd_ui_fieldset_renders"
+  s.version = "0.1"
+  s.authors = ["Yurak Sisa Dream"]
+  s.date    = "2012-05-12"
+  s.email   = ["yurak.sisa.dream@gmail.com"]
+  s.files   = Dir['lib/**/*.rb','views/**/*.erb','i18n/**/*.yml'] 
+  s.description = "Yurak Sisa UI fieldset renders"
+  s.summary = "Yurak Sisa UI fieldset renders"
+  
+  s.add_runtime_dependency "ysd_md_fieldset"
+  
+end
